8e17edf7b3b29f140d9c3457e3f84265a712b9d8
Mr Andrew London <MrAndrewLondon@gmail.com>
2017-08-04 20:51:31 +0100

    Updates 2

--- js/main.js
+++ js/main.js
@@ -19,6 +19,12 @@ function updateWordCount() {
     			return re.test(email);
 			};
 
+			function validateName(name){
+				var checkName = /^[a-zA-Z\s]{3,}$/ig.test(name);
+				if (checkName) 
+					return true;
+			};
+
 			document.getElementById("form").onsubmit = function() {
 				var nameElement = document.getElementById("name")
 				var emailElement = document.getElementById("email")
@@ -27,21 +33,42 @@ function updateWordCount() {
 				var nameCount = nameElement.value.trim().length
 				var messageCount = messageElement.value.length
 
-				if (messageCount === 0) {
-					alert ("Your name is empty...")
-					return false
+				if (!validateName(nameElement.value)) {					
+					alert ("Your name is empty or too short. The Username should be at least three letters long...")
+					return false;
 				}
 
 				if (!validateEmail(emailElement.value)) {
 					alert ("Please enter a valid email address...")
-					return false
+					return false;
 				}
 				
 				if (messageCount === 0) {
 					alert ("Your message is empty...")
-					return false
+					return false;
 				} else if (messageCount > 1000) {
 					alert ("Your message must be less than 1000 characters.")
-					return false
+					return false;
 				}
+
+				return true;
 			};
+
+var hide = document.querySelector('#submit1');
+hide.addEventListener('click' , hideNews);
+
+function hideNews(event) {
+  event.preventDefault();
+
+  var articlesToHide = document.querySelectorAll('.sub-article')
+  articlesToHide.forEach(function(news) {
+    if (news.style.display === 'none') {
+      news.style.display ='block';
+      hide.style.color='#693d3a';
+
+    } else {
+      news.style.display = 'none';
+      hide.style.color = '#d12f2f';
+      }
+  })
+};
